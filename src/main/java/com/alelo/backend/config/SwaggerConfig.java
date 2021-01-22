77bb2c499e13bade3fb13e22ee6904f08064897e
Gleisson <gleissontr@gmail.com>
2021-01-22 01:59:26 -0300

    Configurando e documentando com Swegger

---
+++ src/main/java/com/alelo/backend/config/SwaggerConfig.java
@@ -0,0 +1,36 @@
+package com.alelo.backend.config;
+
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.VendorExtension;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.ArrayList;
+
+import static springfox.documentation.builders.PathSelectors.regex;
+
+public class SwaggerConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.alelo.teste"))
+                .paths(regex("/api.*"))
+                .build()
+                .apiInfo(metaInfo());
+    }
+    public ApiInfo metaInfo(){
+        return new ApiInfo(
+                "Alelo Teste pratico",
+                "Api Reste para cadastro de clientes",
+                "1.0",
+                "Gleisson Quixabeira",
+                new Contact("Gleisson Quixabeira", "(98) 98150-9687","gleissontr@gmail.com"),
+                "",
+                "", new ArrayList<VendorExtension>()
+        );
+    }
+}
--- src/main/java/com/alelo/backend/controller/ClienteController.java
+++ src/main/java/com/alelo/backend/controller/ClienteController.java
@@ -3,6 +3,7 @@ package com.alelo.backend.controller;
 import com.alelo.backend.model.Cliente;
 import com.alelo.backend.model.Dto.ClienteDto;
 import com.alelo.backend.repository.ClienteRepository;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -17,21 +18,26 @@ public class ClienteController {
     private ClienteRepository clienteRepository;
 
     @GetMapping("/clientes")
+    @ApiOperation(value = "Lista de Clientes casdastrados")
     public List<ClienteDto> get(){
         return ClienteDto.covert(clienteRepository.findAll());
     }
+
     @PostMapping("/cliente")
+    @ApiOperation(value = "Cadastrar um Cliente")
     @ResponseStatus(HttpStatus.CREATED)
     public Cliente post(@RequestBody @Valid Cliente cliente){
         return clienteRepository.save(cliente);
     }
 
     @PutMapping("/cliente")
+    @ApiOperation(value = "Alterar um Cliente")
     public Cliente put(@RequestBody @Valid  Cliente cliente){
         return clienteRepository.save(cliente);
     }
 
     @DeleteMapping("/cliente")
+    @ApiOperation(value = "Deletar um Cliente")
     public void delete(@RequestBody @Valid  Cliente cliente){
         clienteRepository.delete(cliente);
     }
